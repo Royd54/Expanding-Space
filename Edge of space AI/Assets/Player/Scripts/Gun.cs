b7b4b6513026ec7310308875d7bf6fa540d9bf0d
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-05-15 11:32:55 +0200

    spread
    i gave the gun spread

--- Edge of space AI/Assets/Player/Scripts/Gun.cs
+++ Edge of space AI/Assets/Player/Scripts/Gun.cs
@@ -55,7 +55,9 @@ public class Gun : MonoBehaviour
             if (firingType == GunEnum.singleShot)
             {
                 cam.Shake((this.transform.position - spawner.position).normalized, 3f, 0.05f);
+                spawner.localRotation = Quaternion.AngleAxis(Random.Range(-spread, spread), spawner.forward);
                 bulletIns = Instantiate(bullet, spawner.position, spawner.rotation);
+                spawner.localRotation = Quaternion.AngleAxis(0, spawner.forward);
                 bulletIns.SendMessage("SetDamage", damage);
                 bulletIns.SendMessage("SetSpeed", bulletVelocity);
                 primaryUse = false;
