282ed2587d8fd1b9932c4cbdec7ecc9cfac407d1
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-05-17 10:25:22 +0200

    knockback
    made a start on the knockback

---
+++ Edge of space/Assets/Player/Scripts/KnockBackPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockBackPoint : MonoBehaviour
+{
+    private GameObject player;
+    
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    
+    void Update()
+    {
+        Vector3 targetDir = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 500 * Time.deltaTime);
+    }
+}
