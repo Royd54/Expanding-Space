2c1998dc80b880caa415fa72b0c64b789c0f25a4
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-05-27 12:40:33 +0200

    made a start on the generator

---
+++ Edge of space/Assets/Dungeon/Scripts/RoomSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    private List<GameObject> spawners = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
