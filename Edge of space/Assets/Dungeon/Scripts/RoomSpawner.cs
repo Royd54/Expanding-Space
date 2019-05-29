b47f5a155a8891329fe5df45016bc02090bb2ca6
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-05-29 15:11:05 +0200

    made all test rooms for dungeon

--- Edge of space/Assets/Dungeon/Scripts/RoomSpawner.cs
+++ Edge of space/Assets/Dungeon/Scripts/RoomSpawner.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    private List<GameObject> spawners = new List<GameObject>();
+    private enum SpawnDir
+    {
+        left,
+        right,
+        top,
+        bottom
+    }
+    [SerializeField] private SpawnDir sd = SpawnDir.left;
+    private RoomSpawner rs;
+    private bool spawned = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rs = GameObject.FindWithTag("Rooms").GetComponent<RoomSpawner>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Invoke("Spawn", 1f);
+    }
+
+    private void Spawn()
+    {
+        if(!spawned)
+        {
+
+        }
     }
 }
---
+++ Edge of space/Assets/Dungeon/Scripts/SpawnRoom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoom : MonoBehaviour
+{
+    [Header("Hallways")]
+    [SerializeField]
+    [Tooltip("All Hallways with a top entry point")]
+    private List<GameObject> hallwayTopEntrys = new List<GameObject>();
+    [SerializeField]
+    [Tooltip("All Hallways with a right entry point")]
+    private List<GameObject> hallwayRightEntrys = new List<GameObject>();
+    [SerializeField]
+    [Tooltip("All Hallways with a bottom entry point")]
+    private List<GameObject> hallwayBottomEntrys = new List<GameObject>();
+    [SerializeField]
+    [Tooltip("All Hallways with a left entry point")]
+    private List<GameObject> hallwayLeftEntrys = new List<GameObject>();
+
+    [Header("Rooms")]
+    [SerializeField]
+    [Tooltip("All Room with a top entry point")]
+    private List<GameObject> roomTopEntrys = new List<GameObject>();
+    [SerializeField]
+    [Tooltip("All Room with a right entry point")]
+    private List<GameObject> roomRightEntrys = new List<GameObject>();
+    [SerializeField]
+    [Tooltip("All Room with a bottom entry point")]
+    private List<GameObject> roomBottomEntrys = new List<GameObject>();
+    [SerializeField]
+    [Tooltip("All Room with a left entry point")]
+    private List<GameObject> roomLeftEntrys = new List<GameObject>();
+}
