d37c8cd8088f326417ef7e66c3e47fa5c81ca939
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-06-03 14:59:37 +0200

    ding

---
+++ Edge of space/Assets/Scripts/Roy/ItemDropCow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropCow : MonoBehaviour
+{
+    public GameObject flesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.GetComponent<cowHealthController>().health <= 0)
+        {
+            Instantiate(flesh, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Edge of space/Assets/Scripts/Tim/LoadingScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    public Animator anim;
+
+    public void SpawnPlayer()
+    {
+        Instantiate(player, this.transform.position, this.transform.rotation);
+        anim = this.transform.parent.GetComponent<Animator>();
+        anim.SetBool("Done", true);
+    }
+}
