ca244b5f144d3ace8f46981d7d200b53208749bf
Royd54 <duponroy@gmail.com>
2019-06-25 13:26:37 +0200

    map changes

---
+++ Edge of space/Assets/Scripts/Roy/waterSpot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterSpot : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject interactKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        interactKey = this.transform.Find("E").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            if (Vector2.Distance(transform.position, player.transform.position) < 5)
+            {
+                interactKey.SetActive(true);
+                if (Input.GetKey(KeyCode.E))
+                {
+                    player.GetComponent<PlayerStats>().SetWater(player.GetComponent<PlayerStats>().GetWater() + 25f);
+                }
+            }
+            else
+            {
+                interactKey.SetActive(false);
+            }
+        }
+        catch
+        {
+            //do nothing
+        }
+    }
+}
