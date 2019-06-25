7605e75c10be9a260ef063aa23fa0f0596c9e537
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-06-25 20:51:02 +0200

    ding

--- Edge of space/Assets/Scripts/Tim/PopUpText.cs
+++ Edge of space/Assets/Scripts/Tim/PopUpText.cs
@@ -7,16 +7,17 @@ public class PopUpText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textOutput;
     private int tutorialIndex;
-    [SerializeField] [TextArea] private string tutorial_1, tutorial_2, tutorial_3;
+    [SerializeField] [TextArea] private string tutorial_1, tutorial_2, tutorial_3, tutorial_4, tutorial_5, tutorial_6, tutorial_7, tutorial_8;
 
     // Start is called before the first frame update
     void Start()
     {
-        textOutput.text = "fakka";
+        textOutput.text = " ";
+        textOutput.text = tutorial_1;
     }
 
     private void PopUp()
     {
-
+        textOutput.text = tutorial_1;
     }
 }
